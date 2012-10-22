2ad5b77095dfe13560cf24e26020b616f39e45c1
Tony-iut-valence <tony.chizat@iut-valence.fr>
2012-10-22 11:53:21 +0200

    Création d'une Class TestMessage qui test la class Message et ajout de commentaire a la class TestContact.

--- src/fr/iutvalence/java/projets/chat/test/TestContact.java
+++ src/fr/iutvalence/java/projets/chat/test/TestContact.java
@@ -2,7 +2,8 @@ package fr.iutvalence.java.projets.chat.test;
 import fr.iutvalence.java.projets.chat.*;
 
 /**
- * class qui test la class contact
+ * class qui test la class Contact:
+ * crée un contact puis affiche sont nom, ça class et puis applique la methode tostring
  * @author chizatto
  *
  */
@@ -12,8 +13,7 @@ public class TestContact {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Contact paul;
-		paul= new Contact("paul","12:36:21:45:69");
+		Contact paul= new Contact("paul","12:36:21:45:69");
 		System.out.println(paul.getNomContact());
 		System.out.println(paul.getAdrMac());
 		System.out.println(paul.toString());
